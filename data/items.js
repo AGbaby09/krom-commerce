dabc70286acc7e6ab7a9ad42749567c9074600d1
AGbaby09 <agbotsem@gmail.com>
2023-10-21 18:32:14 +0000

    shopping

---
+++ data/items.js
@@ -0,0 +1,167 @@
+const products = [
+    {
+        prodImg:"./assets/images/bag0.jpg",
+        prodName:"Tokyo Ghoul",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag1.jpg",
+        prodName:"Adidas Red",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag2.jpg",
+        prodName:"Adidas B/W",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag3.jpg",
+        prodName:"Colorful Polka",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/classic-brown.JPG",
+        prodName:"Classic Brown",
+        prodCost: 190,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag0.jpg",
+        prodName:"Tokyo Ghoul",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag1.jpg",
+        prodName:"Adidas Red",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag2.jpg",
+        prodName:"Adidas B/W",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag3.jpg",
+        prodName:"Colorful Polka",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/classic-brown.JPG",
+        prodName:"Classic Brown",
+        prodCost: 190,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag0.jpg",
+        prodName:"Tokyo Ghoul",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag1.jpg",
+        prodName:"Adidas Red",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag2.jpg",
+        prodName:"Adidas B/W",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag3.jpg",
+        prodName:"Colorful Polka",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/classic-brown.JPG",
+        prodName:"Classic Brown",
+        prodCost: 190,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag0.jpg",
+        prodName:"Tokyo Ghoul",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag1.jpg",
+        prodName:"Adidas Red",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag2.jpg",
+        prodName:"Adidas B/W",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/bag3.jpg",
+        prodName:"Colorful Polka",
+        prodCost: 60,
+        store: "thrift",
+    },
+    {
+        prodImg:"./assets/images/classic-brown.JPG",
+        prodName:"Classic Brown",
+        prodCost: 190,
+        store: "thrift",
+    },
+    
+]
+
+let items_list = document.getElementById("items-list");
+
+displayItems(products);
+
+function displayItems(products){
+    let displayMenu = products.map((product)=>{
+        
+        return `<div class="card rel hidden">
+                    <div id="item-img" class="center">
+                        <img src=${product.prodImg} alt="">
+                    </div>
+                    <div id="tag" class="abs font2">
+                    <button class="like-heart abs center" title="like">
+                    <i class='bx bx-heart' ></i>
+                    </button>
+                    <h3 id="tag-name">${product.prodName}</h3>
+                    <p id="tag-price">GH₵ ${product.prodCost}.00</p>
+                    </div>
+                    <button id="toCart" class="abs center">Add to cart <i class='bx bxs-cart-add'></i></button>
+                </div>`;
+            })
+            displayMenu = displayMenu.join("");
+            items_list.innerHTML = displayMenu
+        }
+
+        
+let like_hearts = document.querySelectorAll(".like-heart");
+
+like_hearts.forEach( like_heart => {
+    like_heart.addEventListener("click", ()=>{
+        like_heart.getElementsByTagName("i")[0].classList.contains("bx-heart") ? like_heart.querySelector("i").classList.replace("bx-heart", "bxs-heart") : like_heart.querySelector("i").classList.replace("bxs-heart","bx-heart");
+    })
+})
+
+let cards = document.querySelectorAll(".card")
+cards.forEach(card=>{
+    card.querySelector("#item-img").addEventListener("click", (e)=>{
+        modalArticle.classList.replace("invisible", "visible")
+        modalImg.setAttribute("src", e.target.getAttribute("src"))
+        modalName.innerText = card.querySelector("#tag").querySelector("#tag-name").textContent;
+        modalCost.innerText = card.querySelector("#tag").querySelector("#tag-price").textContent;
+    })
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
-let navActions = document.querySelectorAll("header nav #bottom #icons button")
-
 // LOGIN
 let closeLogin = document.getElementById("close-login")
 let loginArticle = document.getElementById("login-article")
@@ -10,7 +8,41 @@ let loginButton = document.getElementById("login-button")
 let closeSignup = document.getElementById("close-signup")
 let signupArticle = document.getElementById("signup-article")
 let signupButton = document.getElementById("signup-button")
+let signupLink = document.getElementById("signup-link")
 // END OF SIGNUP
+
+// CART
+let closeCart = document.getElementById("close-cart")
+let cartArticle = document.getElementById("cart-article")
+function closeCartButton(){
+    cartArticle.classList.replace("visible", "invisible")
+}
+function openCartButton(){
+    cartArticle.classList.replace("invisible", "visible")
+}
+// END OF CART
+
+// WISH
+let closeWish = document.getElementById("close-wish")
+let wishArticle = document.getElementById("wish-article")
+function closeWishButton(){
+    wishArticle.classList.replace("visible", "invisible")
+}
+function openWishButton(){
+    wishArticle.classList.replace("invisible", "visible")
+}
+// END OF WISH
+
+// MODAL
+let closeModal = document.getElementById("close-modal")
+let modalArticle = document.getElementById("modal-article")
+let modalName = document.getElementById("modal-name")
+let modalCost = document.getElementById("modal-cost")
+let modalImg = document.getElementById("modal-img").querySelector("img")
+function closeModals(){
+    modalArticle.classList.replace("visible", "invisible")
+}
+// END OF MODAL
 
 // CAROUSEL
 let carousels = document.querySelectorAll(".slide-me");
@@ -21,6 +53,9 @@ const carouselText = document.getElementById('carouselText')
 function login(){
     loginButton.addEventListener("click", ()=>{
         loginArticle.classList.remove("invisible")
+    })
+    signupLink.addEventListener("click", ()=>{
+        signupArticle.classList.remove("invisible")
     })
 }
 function logit(){
@@ -58,7 +93,6 @@ function carouselMe(){
             carouselText.textContent = texts[-(carouselCount)]
             carousel.style.transform = `translateX(${carouselCount}00%)`;
         }, 5000)
-
     })
 }
 carouselMe();
